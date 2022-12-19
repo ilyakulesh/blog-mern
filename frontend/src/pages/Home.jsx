2a9e4b957732441089c0f17e05e3091cc85c8e82
Ilya Kulesh <ilyakulesh1@yandex.by>
2022-12-19 10:32:08 +0300

    added extraReducers to postsSlice

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,22 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Grid from "@mui/material/Grid";
 
-import axios from "../axios";
-
 import { Post } from "../components/Post";
 import { TagsBlock } from "../components/TagsBlock";
 import { CommentsBlock } from "../components/CommentsBlock";
+import { fetchPosts } from "../redux/slices/posts";
 
 export const Home = () => {
+  const dispatch = useDispatch();
+  const { posts, tags } = useSelector((state) => state.posts);
+
+  const isPostsLoading = posts.status === "loading";
+
   React.useEffect(() => {
-    axios.get("/posts");
+    dispatch(fetchPosts());
   }, []);
   return (
     <>
@@ -25,7 +30,7 @@ export const Home = () => {
       </Tabs>
       <Grid container spacing={4}>
         <Grid xs={8} item>
-          {[...Array(5)].map(() => (
+          {(isPostsLoading ? [...Array(5)] : posts.items).map(() => (
             <Post
               id={1}
               title="Roast the code #1 | Rock Paper Scissors"
